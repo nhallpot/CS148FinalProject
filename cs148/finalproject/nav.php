2e914877aa068a1ddd94cc8c406e9501135089de
Noah Hall-Potvin <nhallpot@uvm.edu>
2014-11-23 09:43:31 -0500

    re-configured filesystem after finding faults

---
+++ cs148/finalproject/nav.php
@@ -0,0 +1,35 @@
+<!-- ######################     Main Navigation   ########################## -->
+    <ul class="sidebar-brand">
+            <?php
+            if ($path_parts['filename'] == "home") {
+                print"\t" . '<li class="activePage">Home</li>' . "\n";
+            } else {
+                print"\t" . '<li><a href="home.php">Home</a></li>' . "\n";
+            }
+            if ($path_parts['filename'] == "info") {
+                print"\t\t" . '<li class="activePage">Information</li>' . "\n";
+            } else {
+                print"\t\t" . '<li><a href="info.php">Information</a></li>' . "\n";
+            }
+            if ($path_parts['filename'] == "schedule") {
+                print"\t\t" . '<li class="activePage">Schedule</li>' . "\n";
+            } else {
+                print"\t\t" . '<li><a href="schedule.php">Schedule</a></li>' . "\n";
+            }
+            if ($path_parts['filename'] == "goalSheet") {
+                print"\t\t" . '<li class="activePage">Goal Sheets</li>' . "\n";
+            } else {
+                print"\t\t" . '<li><a href="goalSheet.php">Goal Sheets</a></li>' . "\n";
+            }
+            if ($path_parts['filename'] == "media") {
+                print"\t\t" . '<li class="activePage">Media</li>' . "\n";
+            } else {
+                print"\t\t" . '<li><a href="media.php">Media</a></li>' . "\n";
+            }
+            if ($path_parts['filename'] == "contact") {
+                print"\t\t" . '<li class="activePage">Contact Us</li>' . "\n";
+            } else {
+                print"\t\t" . '<li><a href="contact.php">Contact Us</a></li>' . "\n";
+            }
+            ?>
+            </ul>
