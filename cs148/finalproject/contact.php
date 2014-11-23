744e7fc4f4ffbca84f3611a3497d66697ca55c01
Noah Hall-Potvin <nhallpot@uvm.edu>
2014-11-23 11:16:00 -0500

    added generic forms to each page

--- cs148/finalproject/contact.php
+++ cs148/finalproject/contact.php
@@ -14,9 +14,9 @@ include 'top.php';
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Simple Sidebar</h1>
-                    <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-                    <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                    <h1>Contact Noah Hall-Potvin </h1>
+                    <!-- put text here -->
+                    
                     <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
                 </div>
             </div>
