2231561ac287ed610ab00fae3833b89fa88403b4
Noah Hall-Potvin <nhallpot@uvm.edu>
2014-11-22 22:02:31 -0500

    updated home and nav page

--- nav.php
+++ nav.php
@@ -1,10 +1,36 @@
 <!-- ######################     Main Navigation   ########################## -->
-<nav>
+<nav class="navigation">
             <ul>
             <?php
-            if ($path_parts['filename'] == "form") {
-                
-                print"\t" . '<li><a href="form.php">Try Again</a></li>' . "\n";
+            if ($path_parts['filename'] == "home") {
+                print"\t" . '<li class="activePage">Home</li>' . "\n";
+            } else {
+                print"\t" . '<li><a href="home.php">Home</a></li>' . "\n";
+            }
+            if ($path_parts['filename'] == "info") {
+                print"\t\t" . '<li class="activePage">Information</li>' . "\n";
+            } else {
+                print"\t\t" . '<li><a href="info.php">Information</a></li>' . "\n";
+            }
+            if ($path_parts['filename'] == "schedule") {
+                print"\t\t" . '<li class="activePage">Schedule</li>' . "\n";
+            } else {
+                print"\t\t" . '<li><a href="schedule.php">Schedule</a></li>' . "\n";
+            }
+            if ($path_parts['filename'] == "goalSheet") {
+                print"\t\t" . '<li class="activePage">Goal Sheets</li>' . "\n";
+            } else {
+                print"\t\t" . '<li><a href="goalSheet.php">Goal Sheets</a></li>' . "\n";
+            }
+            if ($path_parts['filename'] == "media") {
+                print"\t\t" . '<li class="activePage">Media</li>' . "\n";
+            } else {
+                print"\t\t" . '<li><a href="media.php">Media</a></li>' . "\n";
+            }
+            if ($path_parts['filename'] == "contact") {
+                print"\t\t" . '<li class="activePage">Contact Us</li>' . "\n";
+            } else {
+                print"\t\t" . '<li><a href="contact.php">Contact Us</a></li>' . "\n";
             }
             ?>
             </ul>
