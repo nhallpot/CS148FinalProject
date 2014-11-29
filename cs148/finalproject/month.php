267faa198b07a5f2404c53e2223caff39c719793
Noah Hall-Potvin <nhallpot@uvm.edu>
2014-11-29 13:28:34 -0500

    code not compiling, restarting computer

--- cs148/finalproject/month.php
+++ cs148/finalproject/month.php
@@ -1,12 +1,13 @@
 <?php
 include 'top.php';
+include 'connectToDatabase.php';
 ?>
 <div id="wrapper">
     <div id="sidebar-wrapper">
         <!-- the div below will go below the logo and contain site navigation -->
-            <?php
-            include 'nav.php';
-            ?>
+        <?php
+        include 'nav.php';
+        ?>
 
     </div>
     <!-- Page Content -->
@@ -16,48 +17,123 @@ include 'top.php';
                 <div class="col-lg-12">
                     <h1>Begin Your Month Count</h1>
                     <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
-
-                <?php
-                include 'jquery.php';// this is for front end stuff
-
-                ?>
-                      <!-- pull info from tblItem -->
-                <?php
-                // print $thisDatabase;
-                        //Build query
-                $query = 'select pmkItemId as "Item ID", fldItemName as "Item Name",fldTotalOnHand as "Total On Hand",fldDepartment as "Department",fnkItemMonthCount as "Month To Be Counted" ';
-                $query .= "FROM tblItem ";
-                $query .= 'WHERE fldApproved=1 '; // We just want to show the items that have been confirmed by an admint
-                //$query .= 'and fldDepartment like ? ';
-                $query .= 'order by pmkItemId';
-                //$data = array($subject ."%",$number ."%",$building ."%",$startTime ."%",$typeOfClass ."%",$professor ."%");
-                $keys = array_keys($row);
-
-                ////        PUT CODE FROM Q01.PHP
-                /* ##### Step three
-                * Execute the query
-
-                *      */
-
-                $results = $thisDatabase->select($query);
-
-
-                /* ##### Step four
-                * prepare output and loop through array
-
-                *      */
-
+                    <?php
+                    include 'jquery.php'; // this is for front end stuff
                     ?>
-                        <form action="<?php print $phpSelf; ?>"
-                              method="post"
-                              id="frmSelectMonth">
-                            <fieldset class="wrapper">
-                                <fieldset class="wrapperTwo">
-                                    <legend>Select the Month of Your Count</legend>
-                                    <fieldset class="contact">
+                    <!-- First form here -->
+                    <?php
+                    //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+                    //
+                    // SECTION: 1 Initialize variables
+                    //
+                    // SECTION: 1a.
+                    // variables for the classroom purposes to help find errors.
+                    $debug = false;
+                    if (isset($_GET["debug"])) { // ONLY do this in a classroom environment
+                        $debug = true;
+                    }
+                    if ($debug)
+                        print "<p>DEBUG MODE IS ON</p>";
+                    //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+                    //
+                    // SECTION: 1b Security
+                    //
+                    // define security variable to be used in SECTION 2a.
+                    $yourURL = $domain . $phpSelf;
+                    //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+                    //
+                    // SECTION: 1c form variables
+                    //
+                    // Initialize variables one for each form element
+                    // in the order they appear on the form
+                    $month = "";
+                    //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+                    //
+                    // SECTION: 1d form error flags
+                    //
+                    // Initialize Error Flags one for each form element we validate
+                    // in the order they appear in section 1c.
+                    $monthError = false;
+                    //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
+                    //
+                    // SECTION: 1e misc variables
+                    //
+                    // create array to hold error messages filled (if any) in 2d displayed in 3c.
+                    $errorMsg = array();
 
-                                        <label for="btnMonth">Month of Count
-                                        
+                    // array used to hold form values that will be written to a CSV file
+                    $dataRecord = array();
+
+                    // SECTION: 2 Process for when the form is submitted
+                    //
+                    if (isset($_POST["btnDisplayInventory"])) {
+                        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                        //
+                         // SECTION: 2a Security
+                        // 
+                        if (!securityCheck(true)) {
+                            $msg = "<p>Sorry you cannot access this page. ";
+                            $msg.= "Security breach detected and reported</p>";
+                            die($msg);
+                        }
+
+                        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                        //
+                        // SECTION: 2b Sanitize (clean) data 
+                        // remove any potential JavaScript or html code from users input on the
+                        // form. Note it is best to follow the same order as declared in section 1c.
+                        $month = $_POST(["radMonth"]);
+                        $dataRecord[] = $firstName;
+                        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                        //
+                        // SECTION: 2c Validation
+                        //                         // Validation section. Check each value for possible errors, empty or
+                        // not what we expect. You will need an IF block for each element you will
+                        // check (see above section 1c and 1d). The if blocks should also be in the
+                        // order that the elements appear on your form so that the error messages
+                        // will be in the order they appear. errorMsg will be displayed on the form
+                        // see section 3b. The error flag ($emailERROR) will be used in section 3c.
+                        if ($month == "") {
+                            $errorMsg[] = "Please select a month to do an Inventory Count for!";
+                            $monthError = true;
+                        }
+                        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                        //
+                        // SECTION: 2d Process Form - Passed Validation
+                        //
+                        // Process for when the form passes validation (the errorMsg array is empty)
+                        //
+                        if (!$errorMsg) {
+                            if ($debug)
+                                print "<p>Form is valid</p>";
+                            //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                            // SECTION: 2e Save Data
+                            //
+                        // This block saves the data to a CSV file.
+                            //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                            //
+                            // SECTION: 2f Create message
+                            //
+                            // Create table here
+
+                            print $month;
+
+                            //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                            //
+        // SECTION: Create the new form (similar to edit)
+                        } // end form is valid
+                    }// Ends if form is submitted
+                    ?> <!-- End of first form -->
+                    <form action="<?php print $phpSelf; ?>"
+                          method="post"
+                          id="frmSelectMonth">
+                        <fieldset class="wrapper">
+                            <fieldset class="wrapperTwo">
+                                <legend>Select the Month of Your Count</legend>
+                                <fieldset class="contact">
+
+                                    <label for="btnMonth">Month of Count
+
                                         <input id='radJanuary' type="radio" name="radMonth" value="January" >January
                                         <input id='radFebruary' type="radio" name="radMonth" value="February" >February
                                         <input id='radMarch' type="radio" name="radMonth" value="March" >March
@@ -69,35 +145,33 @@ include 'top.php';
                                         <input id='radSeptember' type="radio" name="radMonth" value="September" >September
                                         <input id='radOctober' type="radio" name="radMonth" value="October" >October
                                         <input id='radNovember' type="radio" name="radMonth" value="November" >November
-                                        
-                                        </select>
-                                        </label>
-                                    
-                                    </fieldset> <!-- ends contact -->
-                                    
-                                </fieldset> <!-- ends wrapper Two -->
-                                
-                                <fieldset class="buttons">
-                                                                       
-                                    <input type="submit" id="btnDisplayInventory" name="btnDisplayInventory" value="Display Inventory" tabindex="900" class="button">
-                                    
-                                </fieldset> <!-- ends buttons -->
-                            </fieldset> <!-- Ends Wrapper -->
-                        </form>
-                        
-                </article>
-                    
-              
+
+                                    </label>
+
+                                </fieldset> <!-- ends contact -->
+
+                            </fieldset> <!-- ends wrapper Two -->
+
+                            <fieldset class="buttons">
+
+                                <input type="submit" id="btnDisplayInventory" name="btnDisplayInventory" value="Display Inventory" tabindex="900" class="button">
+
+                            </fieldset> <!-- ends buttons -->
+                        </fieldset> <!-- Ends Wrapper -->
+                    </form>
+
+
+
                 </div>
             </div>
         </div>
-        
-    
+
+
     </div>
-        <!-- /#page-content-wrapper -->
-    
-    
+    <!-- /#page-content-wrapper -->
+
+
 
 </div>  
-    </body>
+</body>
 </html>
