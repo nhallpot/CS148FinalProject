8eab201a5a108549d0e95e51afa6f6d4caaa24ce
Noah Hall-Potvin <nhallpot@uvm.edu>
2014-11-30 15:26:53 -0500

    done working on the project, cleaning up

--- cs148/finalproject/home.php
+++ cs148/finalproject/home.php
@@ -17,11 +17,22 @@ include 'top.php';
                     <h1>Welcome To Noah Hall-Potvin's</h1>
                     <h2>Inventory Management System</h2>
                     <!-- put text here -->
-                    <?php
-                    $user = htmlentities($_SERVER["REMOTE_USER"], ENT_QUOTES, "UTF-8");
-                    ?>
-                    <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+                    <div class="col-lg-6">
+                    <img src="warehouse.jpg" alt="Warehouse" style="width:600px;height: 400px; margin: 15px">
+                    
+                    </div>
+                    <div class="col-lg-6">
+                    <p>I created this web-site because I am very passionate about finding the best solutions for corporations. While this web-site is very limited in terms of
+                    functionality, it would give a small business the ability to keep track and control their inventory electronically.</p>
+                    <p>Given more time, I would have added a counting system to this website as well. This would be useful to a lot of companies as they wouldn't have to handle
+                    inventory counts on paper, they would just use the same site that they have been keeping track of their inventory already.</p>
+                    <h5>I hope you enjoy the site!</h5>
+                 
+                    </div>
+                <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+    
                 </div>
+                
             </div>
         </div>
     </div>
