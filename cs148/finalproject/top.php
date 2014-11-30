0fcebf9e7b0324501c37ba909de39e4a6fb18c00
Noah Hall-Potvin <nhallpot@uvm.edu>
2014-11-30 11:50:45 -0500

    trying to get styling for ordered list

--- cs148/finalproject/top.php
+++ cs148/finalproject/top.php
@@ -11,12 +11,9 @@
         <!--[if lt IE 9]>
         <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
         <![endif]-->
-
+        <link rel="stylesheet" href="custom.css" type="text/css" media="screen">        
         <link rel="stylesheet" href="bootstrap.css" type="text/css" media="screen">
         <link rel="stylesheet" href="bootstrap.min.css" type="text/css" media="screen">
-        <link rel="stylesheet" href="custom.css" type="text/css" media="screen">
-        <link rel="stylesheet" href="orderedList.css" type="text/css" media="screen">
-
         <link rel="stylesheet" href="simple-sidebar.css" type="text/css" media="screen">
        
 <!--        <style type="text/css">
